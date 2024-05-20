65ac0df392ae0469fac952b8133c705047f44c8e
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-05-20 09:47:38 -0500

    add connection to MongoDB in runServer

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import { app } from "./app";
+import { ConnectMongoDB } from "./mongoDB";
 import { SETTINGS } from "./settings";
 
 const runServer = async () => {
+  const mongoDBConnected = await ConnectMongoDB();
   try {
+    if (!mongoDBConnected) {
+      console.log("Failed to connect MongoDB Atlas");
+      process.exit(1);
+    }
     app.listen(SETTINGS.PORT, () => {
       console.log(`Server runs on ${SETTINGS.PORT}`);
     });
   } catch (error) {
     console.error("Failed to run server:", error);
+    process.exit(1);
   }
 };
 
-runServer()
+runServer();
