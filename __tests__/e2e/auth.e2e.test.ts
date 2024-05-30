a01fec80cd2027aec3f8646ac7954aa85f9d771c
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-05-30 14:03:14 -0500

    finish login user e2e tests for all status codes

--- __tests__/e2e/auth.e2e.test.ts
+++ __tests__/e2e/auth.e2e.test.ts
@@ -36,5 +36,35 @@ describe("auth tests", () => {
         accessToken: expect.any(String),
       });
     });
+
+    it("2 - shouldn't login user and return status code of 401 if unauthorized", async () => {
+      await testManager.createUser();
+
+      const loginInput = {
+        login: "clara",
+        password: "clara10301", //wrong password
+      };
+
+      const res = await request(app)
+        .post(`${SETTINGS.PATH.AUTH}/login`)
+        .send(loginInput)
+        .expect(401);
+    });
+
+    it("3 - shouldn't login user and return  status code of 400 if input has incorrect values", async () => {
+      await testManager.createUser();
+
+      const loginInput = {
+        login: "claraclaraclaraclaraclaraclaraclaraclara", //incorrect input, maxLength: 15
+        password: "clara1030",
+      };
+
+      const res = await request(app)
+        .post(`${SETTINGS.PATH.AUTH}/login`)
+        .send(loginInput)
+        .expect(400);
+    });
   });
+
+  
 });
