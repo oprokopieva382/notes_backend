1ca4f97e36ef48965cb393a788c2765eb6dd31e8
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-06-18 08:52:59 -0500

    add response Time Histogram for DB and REST API

--- src/utils/metrics.ts
+++ src/utils/metrics.ts
@@ -4,6 +4,18 @@ import { logger } from "./logger";
 
 export const app = express();
 
+const restResponseTimeHistogram = new client.Histogram({
+  name: "rest_response_time_duration_seconds",
+  help: "REST API response time in seconds",
+  labelNames: ["method", "route", "status_code"],
+});
+
+const databaseResponseTimeHistogram = new client.Histogram({
+  name: "db_response_time_duration_seconds",
+  help: "Database response time in seconds",
+  labelNames: ["operation", "success"],
+});
+
 export const startMetricsServer = () => {
   const collectDefaultMetrics = client.collectDefaultMetrics;
   collectDefaultMetrics();
