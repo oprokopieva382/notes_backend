eeef8f04ee7f1ea679a2b42fcd5f6b5c90b00560
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-06-18 09:29:12 -0500

    add db metrics for create user and create note

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import swaggerUi from "swagger-ui-express";
 import specs from "./swaggerConfig";
@@ -6,15 +6,29 @@ import { SETTINGS } from "./settings";
 import { usersRouter } from "./features/users";
 import { notesRouter } from "./features/notes";
 import { authRouter } from "./features/auth";
-import { captureMetricsMiddleware, errorHandlerMiddleware } from "./middlewares";
+import { errorHandlerMiddleware } from "./middlewares";
 import { logger } from "./utils/logger";
-import {startMetricsServer} from "./utils/metrics"
+import { restResponseTimeHistogram, startMetricsServer } from "./utils/metrics";
+import responseTime from "response-time";
 
 export const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-//app.use(captureMetricsMiddleware);
+app.use(
+  responseTime((req: Request, res: Response, time: number) => {
+    if (req?.route?.path) {
+      restResponseTimeHistogram.observe(
+        {
+          method: req.method,
+          route: req.route.path,
+          status_code: res.statusCode,
+        },
+        time * 1000
+      );
+    }
+  })
+);
 app.use(SETTINGS.PATH.AUTH, authRouter);
 app.use(SETTINGS.PATH.USERS, usersRouter);
 app.use(SETTINGS.PATH.NOTES, notesRouter);
@@ -22,8 +36,8 @@ app.use(errorHandlerMiddleware);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.get("/", (req, res) => {
-  res.status(200).json({ version: SETTINGS.VERSION});
+  res.status(200).json({ version: SETTINGS.VERSION });
 });
 
 logger.info("Server started");
-startMetricsServer()
+startMetricsServer();
--- src/utils/metrics.ts
+++ src/utils/metrics.ts
@@ -4,13 +4,13 @@ import { logger } from "./logger";
 
 export const app = express();
 
-const restResponseTimeHistogram = new client.Histogram({
+export const restResponseTimeHistogram = new client.Histogram({
   name: "rest_response_time_duration_seconds",
   help: "REST API response time in seconds",
   labelNames: ["method", "route", "status_code"],
 });
 
-const databaseResponseTimeHistogram = new client.Histogram({
+export const databaseResponseTimeHistogram = new client.Histogram({
   name: "db_response_time_duration_seconds",
   help: "Database response time in seconds",
   labelNames: ["operation", "success"],
