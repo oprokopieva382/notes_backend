144a40e6fd6416dd8f6e4d04577ceb435928a118
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-06-21 00:40:52 -0500

    install prettier with husky to the project

--- src/middlewares/errorHandlerMiddleware.ts
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -1,19 +1,19 @@
-import { NextFunction, Request, Response } from "express";
-import { ApiError } from "../helper/api_error";
-import { formatResponse } from "../utils/responseFormatter";
-import { logger } from "../utils/logger";
+import { NextFunction, Request, Response } from 'express';
+import { ApiError } from '../helper/api_error';
+import { formatResponse } from '../utils/responseFormatter';
+import { logger } from '../utils/logger';
 
 export const errorHandlerMiddleware = (
   err: Error | ApiError,
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   logger.error(`${err.message}`);
-  
+
   if (err instanceof ApiError) {
     return formatResponse(res, err.status, {}, err.message, err.errors);
   }
 
-      return formatResponse(res, 500, {}, "Internal Server Error", [err.message]);
+  return formatResponse(res, 500, {}, 'Internal Server Error', [err.message]);
 };
